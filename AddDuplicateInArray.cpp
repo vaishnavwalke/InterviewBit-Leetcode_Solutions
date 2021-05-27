3b3cc0729232ca52f075c09823dc83c43ed29302
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2021-05-27 19:09:26 +0530

    Create AddDuplicateInArray.cpp

---
+++ AddDuplicateInArray.cpp
@@ -0,0 +1,15 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+    int slow=A[0],fast=A[A[0]];
+    while(slow!=fast)
+    {
+        slow=A[slow];
+        fast=A[A[fast]];
+    }
+    fast=0;
+    while(slow!=fast)
+    {
+        slow=A[slow];
+        fast=A[fast];
+    }
+    return slow;
+}
